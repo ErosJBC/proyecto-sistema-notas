dc254866a0bf5ddd4aaf55077d589cfbbd4e8f52
ErosJBC <ebazanc@uni.pe>
2021-10-11 20:20:54 -0500

    Actualizando Login

--- js/Usuario.js
+++ js/Usuario.js
@@ -1,12 +1,24 @@
 'use strict'
 
 class Usuario{
+    _id_user;
     _username;
     _password;
+    _active;
+    _create_date;
+    _update_date;
     
-    constructor(username, password) {
+    constructor(id_user, username, password, active, create_date, update_date) {
+        this._id_user = id_user;
         this._username = username;
         this._password = password;
+        this._active = active;
+        this._create_date = create_date;
+        this._update_date = update_date;
+    }
+
+    get idUser() {
+        return this._id_user;
     }
 
     get username() {
@@ -23,4 +35,24 @@ class Usuario{
         this._password = password;
     }
 
+    get active() {
+        return this._active;
+    }
+    set active(active) {
+        this._active = active;
+    }
+
+    get createDate() {
+        return this._create_date;
+    }
+    set createDate(create_date) {
+        this._create_date = create_date;
+    }
+
+    get updateDate() {
+        return this._update_date;
+    }
+    set password(update_date) {
+        this._update_date = update_date;
+    }
 }
--- js/login.js
+++ js/login.js
@@ -1,26 +1,78 @@
 'use strict'
 
-const user = new Usuario('admin', 'admin')
+const dataUsers = []
 
 const buttonLogin = document.getElementById('btnLogin');
+
+const getUser = async (username, password) => {
+    const api = await fetch(`https://userdb-01-default-rtdb.firebaseio.com/users.json`);
+    const users = await api.json();
+
+    console.log(users);
+
+    users.forEach(user => {
+        if (user != null) {
+            const usr = new Usuario(user.id_user,
+                user.user,
+                user.password,
+                user.active,
+                user.create_date,
+                user.update_date
+            )
+            dataUsers.push(usr)
+        }
+    });
+
+    console.log(dataUsers);
+
+    const validateUser = dataUsers.find(du => {return du.username == username});
+    if(validateUser != null){
+        if(validateUser.active == 1){
+            if(validateUser.username == username && validateUser.password == password){
+                redirectDashboard()
+            }else {
+                alert("Usuario o contraseña incorrecta. Vuelva a ingresar sus credenciales");
+                redirectIndex();
+            }
+        }else{
+            alert("El usuario se encuentra deshabilitado");
+            redirectIndex();
+        }
+    }else {
+        alert("No existe el usuario en el sistema");
+        redirectIndex();
+    }
+}
 
 const redirectDashboard = () => {
     window.location.href = './views/dashboard.html';
 }
 
-const validLogin = (event) => {
+const redirectIndex = () => {
+    window.location.href = '../index.html';
+}
+
+const validateLogin = (event) => {
 
     event.preventDefault();
     const frmLogin= document.forms['frmLogin'];
     const username = frmLogin['inputUser'].value;
     const password = frmLogin['inputPassword'].value;
 
-    if (user.username == username && user.password == password ) {
-        alert('Ingreso éxitoso')
-        setTimeout("redirectDashboard()", 3000);
-    }else {
-        alert('Ingrese un usuario y contraseña válida')
-    }
+    onload(username, password)
 }
 
-buttonLogin.addEventListener('click', validLogin);
+const onload = (username, password) => {
+
+    getUser(username, password);
+
+    const spinner = `
+        <div class="content">
+            <div class="spinner position-relative">
+            </div>
+        </div>`
+    
+    document.getElementById('body').innerHTML = spinner;
+}
+
+buttonLogin.addEventListener('click', validateLogin);
