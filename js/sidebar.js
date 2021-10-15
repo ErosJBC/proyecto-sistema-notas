b855b7f3a1cbbd43b20b6304a8a7452c886c78d3
ErosJBC <ebazanc@uni.pe>
2021-10-15 13:01:28 -0500

    Dándole nuevo estilo a la página

---
+++ js/sidebar.js
@@ -0,0 +1,10 @@
+'use strict'
+
+$(document).ready(() => {
+    $("#toogle-sidebar").click(() => {
+        // $("#sidebar").toggleClass("close");
+        $("#main-container").toggleClass("show");
+    });
+});
+
+
